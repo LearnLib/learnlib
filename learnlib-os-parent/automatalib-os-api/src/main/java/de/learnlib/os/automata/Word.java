c8e3ffa1299ce657fcc69e8d8d66999d690bb27e
Maik Merten <maik.merten@tu-dortmund.de>
2012-11-16 16:00:51 +0100

    initial automata api things

---
+++ learnlib-os-parent/automatalib-os-api/src/main/java/de/learnlib/os/automata/Word.java
@@ -0,0 +1,32 @@
+/* Copyright (C) 2012 TU Dortmund
+   This file is part of LearnLib 
+
+   LearnLib is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License version 3.0 as published by the Free Software Foundation.
+
+   LearnLib is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with LearnLib; if not, see
+   <http://www.gnu.de/documents/lgpl.en.html>.  */
+
+package de.learnlib.os.automata;
+
+/**
+ *
+ * @author merten
+ */
+public interface Word extends Iterable<Symbol> {
+	
+	public void addSymbol(Symbol sym);
+	
+	public Symbol getSymbol(int index);
+	
+	public int size();
+	
+	
+}
