82cffde241a2e531e35f07ca5426ce2c5e60bd64
merten <merten@merten-ThinkPad-Edge-E325>
2013-03-15 10:52:08 +0100

    first implementation of a simulator oracle for mealy machines

---
+++ learnlib-os-parent/learnlib-os-impl/src/main/java/de/learnlib/mealy/oracles/MealySimulatorOracle.java
@@ -0,0 +1,53 @@
+/* Copyright (C) 2013 TU Dortmund
+   This file is part of LearnLib 
+
+   LearnLib is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License version 3.0 as published by the Free Software Foundation.
+
+   LearnLib is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with LearnLib; if not, see
+   <http://www.gnu.de/documents/lgpl.en.html>.  */
+
+package de.learnlib.mealy.oracles;
+
+import de.learnlib.api.MembershipOracle;
+import de.learnlib.api.Query;
+import de.ls5.automata.transout.MealyMachine;
+import de.ls5.words.MutableWord;
+import de.ls5.words.Word;
+import de.ls5.words.impl.ArrayWord;
+import java.util.List;
+
+/**
+ *
+ * @author Maik Merten <maikmerten@googlemail.com>
+ */
+public class MealySimulatorOracle<S, I, T, O> implements MembershipOracle<I, Word<O>> {
+    
+    private MealyMachine<S, I, T, O> mealy;
+    
+    
+    public MealySimulatorOracle(MealyMachine<S, I, T, O> mealyMachine) {
+        this.mealy = mealyMachine;
+    }
+    
+
+    @Override
+    public void processQueries(List<Query<I, Word<O>>> queries) {
+        for(Query<I, Word<O>> q : queries) {
+            Word<I> input = q.getInput();
+            
+            MutableWord<O> output = new ArrayWord<O>();
+            mealy.trace(input, output);
+           
+            q.setOutput(output);
+        }
+    }
+    
+}
