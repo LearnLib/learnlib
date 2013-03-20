ab267beea398618b3c8c0c4023bc7887c9e82142
merten <merten@merten-ThinkPad-Edge-E325>
2013-03-20 12:13:46 +0100

    use automata-example-automata machines for unit test

--- learnlib-os-parent/learnlib-os-algorithms/src/test/java/de/learnlib/lstar/LStarDFATest.java
+++ learnlib-os-parent/learnlib-os-algorithms/src/test/java/de/learnlib/lstar/LStarDFATest.java
@@ -22,10 +22,8 @@ import java.util.List;
 
 import net.automatalib.automata.fsa.DFA;
 import net.automatalib.automata.fsa.impl.FastDFA;
-import net.automatalib.automata.fsa.impl.FastDFAState;
 import net.automatalib.words.Alphabet;
 import net.automatalib.words.Word;
-import net.automatalib.words.impl.FastAlphabet;
 import net.automatalib.words.impl.Symbol;
 
 import org.junit.Test;
@@ -44,50 +42,11 @@ import de.learnlib.lstar.closing.ClosingStrategy;
 import de.learnlib.lstar.dfa.ExtensibleLStarDFA;
 import de.learnlib.oracles.SimulatorOracle;
 
+import static net.automatalib.examples.dfa.ExamplePaulAndMary.*;
+
 public class LStarDFATest extends LearningTest {
 
-	private final static Symbol in_paul = new Symbol("Paul");
-    private final static Symbol in_loves = new Symbol("loves");
-    private final static Symbol in_mary = new Symbol("Mary");
-
-    public FastDFA<Symbol> constructMachine() {
-        
-        Alphabet<Symbol> alpha = new FastAlphabet<>();
-        alpha.add(in_paul);
-        alpha.add(in_loves);
-        alpha.add(in_mary);
-        
-        FastDFA<Symbol> dfa = new FastDFA<>(alpha);
-        
-        FastDFAState s0 = dfa.addInitialState(false),
-                s1 = dfa.addState(false),
-                s2 = dfa.addState(false),
-                s3 = dfa.addState(true),
-                s4 = dfa.addState(false);
-
-        dfa.addTransition(s0, in_paul, s1);
-        dfa.addTransition(s0, in_loves, s4);
-        dfa.addTransition(s0, in_mary, s4);
-        
-        dfa.addTransition(s1, in_paul, s4);
-        dfa.addTransition(s1, in_loves, s2);
-        dfa.addTransition(s1, in_mary, s4);
-        
-        dfa.addTransition(s2, in_paul, s4);
-        dfa.addTransition(s2, in_loves, s4);
-        dfa.addTransition(s2, in_mary, s3);
-        
-        dfa.addTransition(s3, in_paul, s4);
-        dfa.addTransition(s3, in_loves, s4);
-        dfa.addTransition(s3, in_mary, s4);
-        
-        dfa.addTransition(s4, in_paul, s4);
-        dfa.addTransition(s4, in_loves, s4);
-        dfa.addTransition(s4, in_mary, s4);
-
-        return dfa;
-    }
-    
+ 
 	@Test
 	public void testLStar() {
 		FastDFA<Symbol> targetDFA = constructMachine();
