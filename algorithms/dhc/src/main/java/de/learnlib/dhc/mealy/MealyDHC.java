9c4821f26a2ae03a2a3d308a699a3bc372c187e8
Maik Merten <maikmerten@googlemail.com>
2013-03-28 09:46:30 +0100

    minor reformat (tab indent)

--- algorithms/dhc/src/main/java/de/learnlib/dhc/mealy/MealyDHC.java
+++ algorithms/dhc/src/main/java/de/learnlib/dhc/mealy/MealyDHC.java
@@ -42,20 +42,20 @@ import de.learnlib.oracles.DefaultQuery;
  * @author Maik Merten <maikmerten@googlemail.com>
  */
 public class MealyDHC<I, O> implements LearningAlgorithm<MealyMachine<?, I, ?, O>, I, Word<O>>,
-        SupportsCEXHandlerSuffixes<I, Word<O>> {
+		SupportsCEXHandlerSuffixes<I, Word<O>> {
 
-    private Alphabet<I> alphabet;
-    private MembershipOracle<I, Word<O>> oracle;
-    private SimpleAlphabet<Word<I>> splitters = new SimpleAlphabet<>();
-    private FastMealy<I, O> hypothesis;
-    private CEXHandlerSuffixes<I, Word<O>> cexhandler = new CEXHandlerAllSuffixes<>();
-	
+	private Alphabet<I> alphabet;
+	private MembershipOracle<I, Word<O>> oracle;
+	private SimpleAlphabet<Word<I>> splitters = new SimpleAlphabet<>();
+	private FastMealy<I, O> hypothesis;
+	private CEXHandlerSuffixes<I, Word<O>> cexhandler = new CEXHandlerAllSuffixes<>();
+
 	private class QueueElement {
 		private FastMealyState<O> parentState;
 		private QueueElement parentElement;
 		private I transIn;
 		private O transOut;
-		
+
 		private QueueElement(FastMealyState<O> parentState, QueueElement parentElement, I transIn, O transOut) {
 			this.parentState = parentState;
 			this.parentElement = parentElement;
@@ -63,135 +63,134 @@ public class MealyDHC<I, O> implements LearningAlgorithm<MealyMachine<?, I, ?, O
 			this.transOut = transOut;
 		}
 	}
-	
 
-    public MealyDHC(Alphabet<I> alphabet, MembershipOracle<I, Word<O>> oracle) {
-        this.alphabet = alphabet;
-        this.oracle = oracle;
-    }
+	public MealyDHC(Alphabet<I> alphabet, MembershipOracle<I, Word<O>> oracle) {
+		this.alphabet = alphabet;
+		this.oracle = oracle;
+	}
 
-    @Override
-    public void startLearning() {
+	@Override
+	public void startLearning() {
 
-        // the effective alphabet is the concatenation of the real alphabet
-        // wrapped in Words and the list of splitters
-        SimpleAlphabet<Word<I>> effectivealpha = new SimpleAlphabet<>();
-        for (I input : alphabet) {
-            effectivealpha.add(Word.fromLetter(input));
-        }
-        effectivealpha.addAll(splitters);
+		// the effective alphabet is the concatenation of the real alphabet
+		// wrapped in Words and the list of splitters
+		SimpleAlphabet<Word<I>> effectivealpha = new SimpleAlphabet<>();
+		for (I input : alphabet) {
+			effectivealpha.add(Word.fromLetter(input));
+		}
+		effectivealpha.addAll(splitters);
 
-        // initialize structure to store state output signatures
-        Map<List<Word<O>>, FastMealyState<O>> signatures = new HashMap<>();
+		// initialize structure to store state output signatures
+		Map<List<Word<O>>, FastMealyState<O>> signatures = new HashMap<>();
 
-        // set up new hypothesis machine
-        hypothesis = new FastMealy<>(alphabet);
+		// set up new hypothesis machine
+		hypothesis = new FastMealy<>(alphabet);
 
-        // initialize exploration queue
-        Queue<QueueElement> queue = new LinkedList<>();
-		
+		// initialize exploration queue
+		Queue<QueueElement> queue = new LinkedList<>();
+
 		// first element to be explored represents the initial state with no predecessor
-        queue.add(new QueueElement(null, null, null, null));
+		queue.add(new QueueElement(null, null, null, null));
 
-        while (!queue.isEmpty()) {
-            // get element to be explored from queue
-            QueueElement elem = queue.poll();
+		while (!queue.isEmpty()) {
+			// get element to be explored from queue
+			QueueElement elem = queue.poll();
 
-            // determine access sequence for state
-            Word<I> access = assembleAccessSequence(elem);
+			// determine access sequence for state
+			Word<I> access = assembleAccessSequence(elem);
 
-            // assemble queries
-            ArrayList<DefaultQuery<I, Word<O>>> queries = new ArrayList<>(effectivealpha.size());
-            for (Word<I> suffix : effectivealpha) {
-                queries.add(new DefaultQuery<I, Word<O>>(access, suffix));
-            }
+			// assemble queries
+			ArrayList<DefaultQuery<I, Word<O>>> queries = new ArrayList<>(effectivealpha.size());
+			for (Word<I> suffix : effectivealpha) {
+				queries.add(new DefaultQuery<I, Word<O>>(access, suffix));
+			}
 
-            // retrieve answers
-            oracle.processQueries(queries);
+			// retrieve answers
+			oracle.processQueries(queries);
 
-            // assemble output signature
-            List<Word<O>> sig = new ArrayList<>(effectivealpha.size());
-            for (DefaultQuery<I, Word<O>> query : queries) {
-                sig.add(query.getOutput());
-            }
-			
+			// assemble output signature
+			List<Word<O>> sig = new ArrayList<>(effectivealpha.size());
+			for (DefaultQuery<I, Word<O>> query : queries) {
+				sig.add(query.getOutput());
+			}
+
 			FastMealyState<O> sibling = signatures.get(sig);
 
-            if (sibling != null) {
-                // this element does not possess a new output signature
+			if (sibling != null) {
+				// this element does not possess a new output signature
 				// create a transition from parent state to sibling
 				hypothesis.addTransition(elem.parentState, elem.transIn, sibling, elem.transOut);
-            } else {
-                // this is actually an observably distinct state! Progress!
-                // Create state and connect via transition to parent
+			} else {
+				// this is actually an observably distinct state! Progress!
+				// Create state and connect via transition to parent
 				FastMealyState<O> state = elem.parentElement == null ? hypothesis.addInitialState() : hypothesis.addState();
-				if(elem.parentElement != null) {
+				if (elem.parentElement != null) {
 					hypothesis.addTransition(elem.parentState, elem.transIn, state, elem.transOut);
 				}
 				signatures.put(sig, state);
-				
-                scheduleSuccessors(elem, state, queue, sig);
-            }
-        }
-    }
+
+				scheduleSuccessors(elem, state, queue, sig);
+			}
+		}
+	}
 
 	private Word<I> assembleAccessSequence(QueueElement elem) {
-        List<I> sequence = new ArrayList<>();
-        
-        QueueElement pre = elem.parentElement;
-        I sym = elem.transIn;
-        while(pre != null && sym != null) {
-            sequence.add(sym);
+		List<I> sequence = new ArrayList<>();
+
+		QueueElement pre = elem.parentElement;
+		I sym = elem.transIn;
+		while (pre != null && sym != null) {
+			sequence.add(sym);
 			sym = pre.transIn;
-            pre = pre.parentElement;
-        }
+			pre = pre.parentElement;
+		}
 
 		Collections.reverse(sequence);
-        return Word.fromList(sequence);
-    }
+		return Word.fromList(sequence);
+	}
 
-    private void scheduleSuccessors(QueueElement elem, FastMealyState<O> state, Queue<QueueElement> queue, List<Word<O>> sig) throws IllegalArgumentException {
-        for (int i = 0; i < alphabet.size(); ++i) {
-            // retrieve I/O for transition
-            I input = alphabet.getSymbol(i);
-            O output = sig.get(i).getSymbol(0);
-			
-            // create successor element and schedule for exploration
-            queue.add(new QueueElement(state, elem, input, output));
-        }
-    }
+	private void scheduleSuccessors(QueueElement elem, FastMealyState<O> state, Queue<QueueElement> queue, List<Word<O>> sig) throws IllegalArgumentException {
+		for (int i = 0; i < alphabet.size(); ++i) {
+			// retrieve I/O for transition
+			I input = alphabet.getSymbol(i);
+			O output = sig.get(i).getSymbol(0);
 
-    @Override
-    public boolean refineHypothesis(DefaultQuery<I, Word<O>> ceQuery) {
-        if (hypothesis == null) {
-            throw new IllegalStateException("No hypothesis learned yet");
-        }
+			// create successor element and schedule for exploration
+			queue.add(new QueueElement(state, elem, input, output));
+		}
+	}
 
-        int oldsize = hypothesis.size();
+	@Override
+	public boolean refineHypothesis(DefaultQuery<I, Word<O>> ceQuery) {
+		if (hypothesis == null) {
+			throw new IllegalStateException("No hypothesis learned yet");
+		}
 
-        ArrayList<Word<I>> suffixes = new ArrayList<>();
-        cexhandler.createSuffixes(ceQuery, suffixes);
-        for (Word<I> suffix : suffixes) {
-            if (suffix.size() > 1 && !splitters.contains(suffix)) {
-                splitters.add(suffix);
-            }
-        }
+		int oldsize = hypothesis.size();
 
-        startLearning();
+		ArrayList<Word<I>> suffixes = new ArrayList<>();
+		cexhandler.createSuffixes(ceQuery, suffixes);
+		for (Word<I> suffix : suffixes) {
+			if (suffix.size() > 1 && !splitters.contains(suffix)) {
+				splitters.add(suffix);
+			}
+		}
 
-        return oldsize != hypothesis.size();
-    }
+		startLearning();
 
-    @Override
-    public MealyMachine<?, I, ?, O> getHypothesisModel() {
-        if (hypothesis == null) {
-            throw new IllegalStateException("No hypothesis learned yet");
-        }
-        return (MealyMachine<?, I, ?, O>) hypothesis;
-    }
+		return oldsize != hypothesis.size();
+	}
 
-    @Override
-    public void setCEXHandlerSuffixes(CEXHandlerSuffixes<I, Word<O>> handler) {
-        this.cexhandler = handler;
-    }
+	@Override
+	public MealyMachine<?, I, ?, O> getHypothesisModel() {
+		if (hypothesis == null) {
+			throw new IllegalStateException("No hypothesis learned yet");
+		}
+		return (MealyMachine<?, I, ?, O>) hypothesis;
+	}
+
+	@Override
+	public void setCEXHandlerSuffixes(CEXHandlerSuffixes<I, Word<O>> handler) {
+		this.cexhandler = handler;
+	}
 }
--- algorithms/dhc/src/test/java/de/learnlib/dhc/mealy/MealyDHCTest.java
+++ algorithms/dhc/src/test/java/de/learnlib/dhc/mealy/MealyDHCTest.java
@@ -14,7 +14,6 @@
  * License along with LearnLib; if not, see
  * <http://www.gnu.de/documents/lgpl.en.html>.
  */
-
 package de.learnlib.dhc.mealy;
 
 import static de.learnlib.examples.mealy.ExampleGrid.constructMachine;
@@ -38,98 +37,96 @@ import de.learnlib.oracles.SimulatorOracle;
  * @author merten
  */
 public class MealyDHCTest {
-    
-    @Test
-    public void testMealyDHCGrid() {
-        
-        final int xsize = 5;
-        final int ysize = 5;
-        
-        FastMealy<Symbol, Integer> fm = constructMachine(xsize, ysize);
-        Alphabet<Symbol> alphabet = fm.getInputAlphabet();
-        
-        
-        SimulatorOracle<Symbol, Word<Integer>> simoracle = new SimulatorOracle<>(fm);
-        
-        MealyDHC<Symbol, Integer> dhc = new MealyDHC<>(alphabet, simoracle);
-        
-        dhc.startLearning();
-        MealyMachine<?, Symbol, ?, Integer> hypo = dhc.getHypothesisModel();
-        
-        Assert.assertEquals(hypo.size(), (xsize * ysize), "Mismatch in size of learned hypothesis");
-        
-    }
-    
-    @Test
-    public void testMealyDHCStack() {
-        
-        FastMealy<Symbol, String> fm = ExampleStack.constructMachine();
-        Alphabet<Symbol> alphabet = fm.getInputAlphabet();
-        
-        SimulatorOracle<Symbol, Word<String>> simoracle = new SimulatorOracle<>(fm);
-        
-        MealyDHC<Symbol, String> dhc = new MealyDHC<>(alphabet, simoracle);
-        
-        dhc.startLearning();
-        
-        MealyMachine<?, Symbol, ?, String> hypo = dhc.getHypothesisModel();
-        
-        // for this example the first hypothesis should have two states
-        Assert.assertEquals(hypo.size(), 2, "Mismatch in size of learned hypothesis");
-        
-        SimulatorEQOracle<Symbol, Word<String>> eqoracle = new SimulatorEQOracle<>(fm);
-        
-        DefaultQuery<Symbol, Word<String>> cexQuery = eqoracle.findCounterExample(hypo, alphabet);
-        
-        // a counterexample has to be found
-        Assert.assertNotNull(cexQuery, "No counterexample found for incomplete hypothesis");
-        
-        boolean refined = dhc.refineHypothesis(cexQuery);
-        
-        // the counterexample has to lead to a refinement
-        Assert.assertTrue(refined, "No refinement reported by learning algorithm");
-        
-        hypo = dhc.getHypothesisModel();
-        
-        // the refined hypothesis should now have the correct size
-        Assert.assertEquals(hypo.size(), fm.size(), "Refined hypothesis does not have correct size");
 
-        // no counterexample shall be found now
-        cexQuery = eqoracle.findCounterExample(hypo, alphabet);
-        Assert.assertNull(cexQuery, "Counterexample found despite correct model size");
-        
-        
-    }
-	
 	@Test
-    public void testMealyDHCCoffee() {
-        
-        FastMealy<Symbol, String> fm = ExampleCoffeeMachine.constructMachine();
-        Alphabet<Symbol> alphabet = fm.getInputAlphabet();
-        
-        SimulatorOracle<Symbol, Word<String>> simoracle = new SimulatorOracle<>(fm);
-        SimulatorEQOracle<Symbol, Word<String>> eqoracle = new SimulatorEQOracle<>(fm);
-		
-        MealyDHC<Symbol, String> dhc = new MealyDHC<>(alphabet, simoracle);
-        
-        int rounds = 0;
+	public void testMealyDHCGrid() {
+
+		final int xsize = 5;
+		final int ysize = 5;
+
+		FastMealy<Symbol, Integer> fm = constructMachine(xsize, ysize);
+		Alphabet<Symbol> alphabet = fm.getInputAlphabet();
+
+
+		SimulatorOracle<Symbol, Word<Integer>> simoracle = new SimulatorOracle<>(fm);
+
+		MealyDHC<Symbol, Integer> dhc = new MealyDHC<>(alphabet, simoracle);
+
+		dhc.startLearning();
+		MealyMachine<?, Symbol, ?, Integer> hypo = dhc.getHypothesisModel();
+
+		Assert.assertEquals(hypo.size(), (xsize * ysize), "Mismatch in size of learned hypothesis");
+
+	}
+
+	@Test
+	public void testMealyDHCStack() {
+
+		FastMealy<Symbol, String> fm = ExampleStack.constructMachine();
+		Alphabet<Symbol> alphabet = fm.getInputAlphabet();
+
+		SimulatorOracle<Symbol, Word<String>> simoracle = new SimulatorOracle<>(fm);
+
+		MealyDHC<Symbol, String> dhc = new MealyDHC<>(alphabet, simoracle);
+
+		dhc.startLearning();
+
+		MealyMachine<?, Symbol, ?, String> hypo = dhc.getHypothesisModel();
+
+		// for this example the first hypothesis should have two states
+		Assert.assertEquals(hypo.size(), 2, "Mismatch in size of learned hypothesis");
+
+		SimulatorEQOracle<Symbol, Word<String>> eqoracle = new SimulatorEQOracle<>(fm);
+
+		DefaultQuery<Symbol, Word<String>> cexQuery = eqoracle.findCounterExample(hypo, alphabet);
+
+		// a counterexample has to be found
+		Assert.assertNotNull(cexQuery, "No counterexample found for incomplete hypothesis");
+
+		boolean refined = dhc.refineHypothesis(cexQuery);
+
+		// the counterexample has to lead to a refinement
+		Assert.assertTrue(refined, "No refinement reported by learning algorithm");
+
+		hypo = dhc.getHypothesisModel();
+
+		// the refined hypothesis should now have the correct size
+		Assert.assertEquals(hypo.size(), fm.size(), "Refined hypothesis does not have correct size");
+
+		// no counterexample shall be found now
+		cexQuery = eqoracle.findCounterExample(hypo, alphabet);
+		Assert.assertNull(cexQuery, "Counterexample found despite correct model size");
+
+
+	}
+
+	@Test
+	public void testMealyDHCCoffee() {
+
+		FastMealy<Symbol, String> fm = ExampleCoffeeMachine.constructMachine();
+		Alphabet<Symbol> alphabet = fm.getInputAlphabet();
+
+		SimulatorOracle<Symbol, Word<String>> simoracle = new SimulatorOracle<>(fm);
+		SimulatorEQOracle<Symbol, Word<String>> eqoracle = new SimulatorEQOracle<>(fm);
+
+		MealyDHC<Symbol, String> dhc = new MealyDHC<>(alphabet, simoracle);
+
+		int rounds = 0;
 		DefaultQuery<Symbol, Word<String>> counterexample = null;
 		do {
-			if(counterexample == null) {
+			if (counterexample == null) {
 				dhc.startLearning();
 			} else {
 				Assert.assertTrue(dhc.refineHypothesis(counterexample), "Counterexample did not refine hypothesis");
 			}
-			
-			counterexample = eqoracle.findCounterExample(dhc.getHypothesisModel(), alphabet);
-			
-			Assert.assertTrue(rounds++ < fm.size(), "Learning took more rounds than states in target model");
-			
-		} while(counterexample != null);
-		
-        Assert.assertEquals(dhc.getHypothesisModel().size(), fm.size(), "Mismatch in size of learned hypothesis and target model");
-            
-    }
 
-    
+			counterexample = eqoracle.findCounterExample(dhc.getHypothesisModel(), alphabet);
+
+			Assert.assertTrue(rounds++ < fm.size(), "Learning took more rounds than states in target model");
+
+		} while (counterexample != null);
+
+		Assert.assertEquals(dhc.getHypothesisModel().size(), fm.size(), "Mismatch in size of learned hypothesis and target model");
+
+	}
 }
