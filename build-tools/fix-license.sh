b5992646907a028003a70f8cb967f36e46a877a3
Malte Isberner <malte.isberner@cs.tu-dortmund.de>
2013-03-27 18:44:28 -0700

    Fixed missing license headers

--- build-tools/fix-license.sh
+++ build-tools/fix-license.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+LICENSE_HEADER="./src/main/resources/license-header.txt"
+
 if [ "$*" == "" ]; then
 	find .. -name '*.java' | xargs "$0"
 	exit
@@ -9,7 +11,7 @@ for i in "$@"; do
 	license=`head -n 1 "$i" | grep -c "/* Copyright (C) 2013"`
 	if [ "$license" != "1" ]; then
 		echo "File $i misses license header, fixing"
-		cp license-header.txt .tmp
+		cp "$LICENSE_HEADER" .tmp
 		oldlicense=`head -n 1 "$i" | grep -c "/* Copyright (C) 2012"`
 		if [ "$oldlicense" == "1" ]; then
 			echo "Stripping old license from $i"
