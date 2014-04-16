5374028c0e0fc1c7ae9ad8c9a2e57e69d629711b
Malte Isberner <malte.isberner@cs.tu-dortmund.de>
2014-04-16 01:27:16 -0700

    Added missing license headers, removed obsolete files

--- algorithms/features/src/test/java/de/learnlib/algorithms/features/observationtable/writer/SuffixASCIIWriterTest.java
+++ algorithms/features/src/test/java/de/learnlib/algorithms/features/observationtable/writer/SuffixASCIIWriterTest.java
@@ -1,12 +1,30 @@
+/* Copyright (C) 2014 TU Dortmund
+ * This file is part of LearnLib, http://www.learnlib.de/.
+ *
+ * LearnLib is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 3.0 as published by the Free Software Foundation.
+ *
+ * LearnLib is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with LearnLib; if not, see
+ * <http://www.gnu.de/documents/lgpl.en.html>.
+ */
 package de.learnlib.algorithms.features.observationtable.writer;
 
 import de.learnlib.algorithms.features.observationtable.OTUtils;
 import de.learnlib.algorithms.features.observationtable.ObservationTable;
 import de.learnlib.algorithms.features.observationtable.reader.SuffixASCIIReader;
 import de.learnlib.algorithms.features.observationtable.writer.otsource.ObservationTableSource;
-import junit.framework.Assert;
+
 import net.automatalib.words.Alphabet;
 import net.automatalib.words.impl.SimpleAlphabet;
+
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class SuffixASCIIWriterTest {
