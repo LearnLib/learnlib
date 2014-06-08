3243b088d2ce401b7cb9081b034d56f871192767
Malte Isberner <malte.isberner@gmail.com>
2014-06-08 12:52:06 -0700

    Fixed copyright headers

--- algorithms/nlstar/src/main/java/de/learnlib/algorithms/nlstar/Inconsistency.java
+++ algorithms/nlstar/src/main/java/de/learnlib/algorithms/nlstar/Inconsistency.java
@@ -1,17 +1,17 @@
 /* Copyright (C) 2014 TU Dortmund
- * This file is part of AutomataLib, http://www.automatalib.net/.
- * 
- * AutomataLib is free software; you can redistribute it and/or
+ * This file is part of LearnLib, http://www.learnlib.de/.
+ *
+ * LearnLib is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License version 3.0 as published by the Free Software Foundation.
- * 
- * AutomataLib is distributed in the hope that it will be useful,
+ *
+ * LearnLib is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
- * License along with AutomataLib; if not, see
+ * License along with LearnLib; if not, see
  * <http://www.gnu.de/documents/lgpl.en.html>.
  */
 package de.learnlib.algorithms.nlstar;
