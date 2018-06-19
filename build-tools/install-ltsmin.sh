9c188a06fcb660b2272784cd8a59d64960375eae
Markus Frohme <markus.frohme@udo.edu>
2018-06-19 16:41:05 +0200

    travis: fix installation of LTSmin

--- build-tools/install-ltsmin.sh
+++ build-tools/install-ltsmin.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-LTSMIN_NAME="ltsmin-v3.0.0-$TRAVIS_OS_NAME.tgz"
+LTSMIN_VERSION=v3.0.0
+LTSMIN_NAME="ltsmin-${LTSMIN_VERSION}-$TRAVIS_OS_NAME.tgz"
 LTSMIN_URL="https://github.com/Meijuh/ltsmin/releases/download/v3.0.0/$LTSMIN_NAME"
 
 # test if we have a cached version
-test -f "$HOME/ltsmin/bin/ltsmin-convert" -a -f "$HOME/ltsmin/bin/etf2lts-mc" && exit 0
+test -f "$HOME/ltsmin/${LTSMIN_VERSION}/bin/ltsmin-convert" -a -f "$HOME/ltsmin/${LTSMIN_VERSION}/bin/etf2lts-mc" && exit 0
 
 # create the directoy where the binaries end up.
 mkdir -p "$HOME/ltsmin"
