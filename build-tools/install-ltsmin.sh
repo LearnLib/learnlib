f7b0feb0dd42cae9d834ddf766ea8cf0391f7aaa
Markus Frohme <markus.frohme@udo.edu>
2018-10-24 15:57:48 +0200

    build-tools: make LTSmin install script windows/macOS compatible

--- build-tools/install-ltsmin.sh
+++ build-tools/install-ltsmin.sh
@@ -3,18 +3,23 @@
 LTSMIN_NAME="ltsmin-${LTSMIN_VERSION}-$TRAVIS_OS_NAME.tgz"
 LTSMIN_URL="https://github.com/${LTSMIN_REPO:-utwente-fmt}/ltsmin/releases/download/$LTSMIN_VERSION/$LTSMIN_NAME"
 
-# test if we have a cached version
-test -f "$HOME/ltsmin/${LTSMIN_VERSION}/bin/ltsmin-convert" -a -f "$HOME/ltsmin/${LTSMIN_VERSION}/bin/etf2lts-mc" && exit 0
+if [ $TRAVIS_OS_NAME = "windows" ]; then
+   FILE_SUFFIX=".exe"
+fi
 
-# create the directoy where the binaries end up.
+# test if we have a cached version
+test -f "$HOME/ltsmin/${LTSMIN_VERSION}/bin/ltsmin-convert${FILE_SUFFIX}" -a -f "$HOME/ltsmin/${LTSMIN_VERSION}/bin/etf2lts-mc${FILE_SUFFIX}" && exit 0
+
+# create the directoy where the binaries and downloads end up.
 mkdir -p "$HOME/ltsmin"
+mkdir -p "$HOME/ltsmin-download"
 
 # download the LTSmin binaries
-wget "$LTSMIN_URL" -P /tmp
+wget "$LTSMIN_URL" -P "$HOME/ltsmin-download"
 
 # the files to extract
-echo ltsmin-convert > /tmp/files
-echo etf2lts-mc >> /tmp/files
+echo ${LTSMIN_VERSION}/bin/ltsmin-convert${FILE_SUFFIX} > $HOME/ltsmin-download/files
+echo ${LTSMIN_VERSION}/bin/etf2lts-mc${FILE_SUFFIX} >> $HOME/ltsmin-download/files
 
 # extract the files
-tar -xf "/tmp/$LTSMIN_NAME" -C "$HOME/ltsmin" --no-anchored --files-from=/tmp/files
+tar -xf "$HOME/ltsmin-download/$LTSMIN_NAME" -C "$HOME/ltsmin" --files-from=$HOME/ltsmin-download/files
