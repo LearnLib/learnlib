5061142ed971f55b14a3caed3b4d5cadaa122445
Markus Frohme <markus.frohme@udo.edu>
2018-11-27 18:48:17 +0100

    simplify Experiment class
    revert most changes introduced by the BBCExperiment class, since that class no longer exists

--- commons/util/src/main/java/de/learnlib/util/Experiment.java
+++ commons/util/src/main/java/de/learnlib/util/Experiment.java
@@ -33,175 +33,155 @@ import net.automatalib.words.Word;
  * runs a learning experiment.
  *
  * @param <A> the automaton type
- * @param <I> the input type
- * @param <D> the output type
  *
  * @author falkhowar
- * @author Jeroen Meijer
  */
 @ParametersAreNonnullByDefault
-public class Experiment<A, I, D> {
+public class Experiment<A> {
 
     public static final String LEARNING_PROFILE_KEY = "Learning";
     public static final String COUNTEREXAMPLE_PROFILE_KEY = "Searching for counterexample";
 
     private static final LearnLogger LOGGER = LearnLogger.getLogger(Experiment.class);
-
+    private final ExperimentImpl<?, ?> impl;
     private boolean logModels;
-
     private boolean profile;
-
     private final Counter rounds = new Counter("learning rounds", "#");
-
     private A finalHypothesis;
 
-    private final LearningAlgorithm<? extends A, I, D> learningAlgorithm;
-
-    private final EquivalenceOracle<? super A, I, D> equivalenceAlgorithm;
-
-    private final Alphabet<I> inputs;
-
-    public Experiment(LearningAlgorithm<? extends A, I, D> learningAlgorithm,
-                      EquivalenceOracle<? super A, I, D> equivalenceAlgorithm,
-                      Alphabet<I> inputs) {
-        this.learningAlgorithm = learningAlgorithm;
-        this.equivalenceAlgorithm = equivalenceAlgorithm;
-        this.inputs = inputs;
-    }
-
-    public boolean isLogModels() {
-        return logModels;
-    }
-
-    public Counter getRounds() {
-        return rounds;
-    }
-
-    public LearningAlgorithm<? extends A, I, D> getLearningAlgorithm() {
-        return learningAlgorithm;
-    }
-
-    public EquivalenceOracle<? super A, I, D> getEquivalenceAlgorithm() {
-        return equivalenceAlgorithm;
-    }
-
-    public Alphabet<I> getInputs() {
-        return inputs;
-    }
-
-    public void setLogModels(boolean logModels) {
-        this.logModels = logModels;
-    }
-
-    public void setProfile(boolean profile) {
-        this.profile = profile;
-    }
-
-    /**
-     * Returns whether this experiment has already been run.
-     *
-     * @return {@code true} if this experiment has been run, {@code false} otherwise.
-     */
-    public boolean isRun() {
-        return this.finalHypothesis != null;
-    }
-
-    protected void init() {
-        rounds.increment();
-        LOGGER.logPhase("Starting round " + rounds.getCount());
-        LOGGER.logPhase("Learning");
-
-        profileStart(LEARNING_PROFILE_KEY);
-        learningAlgorithm.startLearning();
-        profileStop(LEARNING_PROFILE_KEY);
-    }
-
-    protected boolean refineHypothesis() {
-        A hyp = getLearningAlgorithm().getHypothesisModel();
-
-        if (logModels) {
-            LOGGER.logModel(hyp);
-        }
-
-        LOGGER.logPhase("Searching for counterexample");
-
-        profileStart(COUNTEREXAMPLE_PROFILE_KEY);
-        DefaultQuery<I, D> ce = equivalenceAlgorithm.findCounterExample(hyp, getInputs());
-        profileStop(COUNTEREXAMPLE_PROFILE_KEY);
-
-        if (ce != null) {
-            LOGGER.logCounterexample(ce.toString());
-
-            // next round ...
-            rounds.increment();
-            LOGGER.logPhase("Starting round " + getRounds().getCount());
-            LOGGER.logPhase("Learning");
-
-            profileStart(LEARNING_PROFILE_KEY);
-            final boolean refined = learningAlgorithm.refineHypothesis(ce);
-            profileStop(LEARNING_PROFILE_KEY);
-
-            assert refined;
-        }
-
-        return ce != null;
+    public <I, D> Experiment(LearningAlgorithm<? extends A, I, D> learningAlgorithm,
+                             EquivalenceOracle<? super A, I, D> equivalenceAlgorithm,
+                             Alphabet<I> inputs) {
+        this.impl = new ExperimentImpl<>(learningAlgorithm, equivalenceAlgorithm, inputs);
     }
 
     @Nonnull
     public A run() {
-        if (isRun()) {
+        if (this.finalHypothesis != null) {
             throw new IllegalStateException("Experiment has already been run");
         }
 
-        init();
-
-        while (refineHypothesis()) { }
-
-        finalHypothesis = learningAlgorithm.getHypothesisModel();
-
+        finalHypothesis = impl.run();
         return finalHypothesis;
-    }
-
-    protected void setFinalHypothesis(A hyp) {
-        finalHypothesis = hyp;
     }
 
     @Nonnull
     public A getFinalHypothesis() {
-        if (!isRun()) {
+        if (finalHypothesis == null) {
             throw new IllegalStateException("Experiment has not yet been run");
         }
+
         return finalHypothesis;
     }
 
-    protected void profileStart(String taskname) {
+    private void profileStart(String taskname) {
         if (profile) {
             SimpleProfiler.start(taskname);
         }
     }
 
-    protected void profileStop(String taskname) {
+    private void profileStop(String taskname) {
         if (profile) {
             SimpleProfiler.stop(taskname);
         }
     }
 
-    public static class DFAExperiment<I> extends Experiment<DFA<?, I>, I, Boolean> {
+    /**
+     * @param logModels
+     *         flag whether models should be logged
+     */
+    public void setLogModels(boolean logModels) {
+        this.logModels = logModels;
+    }
 
+    /**
+     * @param profile
+     *         flag whether learning process should be profiled
+     */
+    public void setProfile(boolean profile) {
+        this.profile = profile;
+    }
+
+    /**
+     * @return the rounds
+     */
+    @Nonnull
+    public Counter getRounds() {
+        return rounds;
+    }
+
+    private final class ExperimentImpl<I, D> {
+
+        private final LearningAlgorithm<? extends A, I, D> learningAlgorithm;
+        private final EquivalenceOracle<? super A, I, D> equivalenceAlgorithm;
+        private final Alphabet<I> inputs;
+
+        ExperimentImpl(LearningAlgorithm<? extends A, I, D> learningAlgorithm,
+                       EquivalenceOracle<? super A, I, D> equivalenceAlgorithm,
+                       Alphabet<I> inputs) {
+            this.learningAlgorithm = learningAlgorithm;
+            this.equivalenceAlgorithm = equivalenceAlgorithm;
+            this.inputs = inputs;
+        }
+
+        public A run() {
+            rounds.increment();
+            LOGGER.logPhase("Starting round " + rounds.getCount());
+            LOGGER.logPhase("Learning");
+
+            profileStart(LEARNING_PROFILE_KEY);
+            learningAlgorithm.startLearning();
+            profileStop(LEARNING_PROFILE_KEY);
+
+            while (true) {
+                final A hyp = learningAlgorithm.getHypothesisModel();
+
+                if (logModels) {
+                    LOGGER.logModel(hyp);
+                }
+
+                LOGGER.logPhase("Searching for counterexample");
+
+                profileStart(COUNTEREXAMPLE_PROFILE_KEY);
+                DefaultQuery<I, D> ce = equivalenceAlgorithm.findCounterExample(hyp, inputs);
+                profileStop(COUNTEREXAMPLE_PROFILE_KEY);
+
+                if (ce == null) {
+                    return hyp;
+                }
+
+                LOGGER.logCounterexample(ce.getInput().toString());
+
+                // next round ...
+                rounds.increment();
+                LOGGER.logPhase("Starting round " + rounds.getCount());
+                LOGGER.logPhase("Learning");
+
+                profileStart(LEARNING_PROFILE_KEY);
+                final boolean refined = learningAlgorithm.refineHypothesis(ce);
+                profileStop(LEARNING_PROFILE_KEY);
+
+                assert refined;
+            }
+        }
+    }
+
+    public static class DFAExperiment<I> extends Experiment<DFA<?, I>> {
         public DFAExperiment(LearningAlgorithm<? extends DFA<?, I>, I, Boolean> learningAlgorithm,
                              EquivalenceOracle<? super DFA<?, I>, I, Boolean> equivalenceAlgorithm,
                              Alphabet<I> inputs) {
             super(learningAlgorithm, equivalenceAlgorithm, inputs);
         }
+
     }
 
-    public static class MealyExperiment<I, O> extends Experiment<MealyMachine<?, I, ?, O>, I, Word<O>> {
+    public static class MealyExperiment<I, O> extends Experiment<MealyMachine<?, I, ?, O>> {
 
         public MealyExperiment(LearningAlgorithm<? extends MealyMachine<?, I, ?, O>, I, Word<O>> learningAlgorithm,
                                EquivalenceOracle<? super MealyMachine<?, I, ?, O>, I, Word<O>> equivalenceAlgorithm,
                                Alphabet<I> inputs) {
             super(learningAlgorithm, equivalenceAlgorithm, inputs);
         }
+
     }
 }
-
