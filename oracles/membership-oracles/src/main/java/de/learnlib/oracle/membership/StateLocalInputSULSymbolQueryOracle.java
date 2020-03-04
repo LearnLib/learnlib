582d0bf8d08b77221322b71cd9cc021af176a2db
Alexander Bainczyk <alexander.bainczyk@tu-dortmund.de>
2020-03-04 15:00:53 +0100

    add state local sul oracle for symbol-based learners

---
+++ oracles/membership-oracles/src/main/java/de/learnlib/oracle/membership/StateLocalInputSULSymbolQueryOracle.java
@@ -0,0 +1,55 @@
+/* Copyright (C) 2013-2020 TU Dortmund
+ * This file is part of LearnLib, http://www.learnlib.de/.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.learnlib.oracle.membership;
+
+import de.learnlib.api.StateLocalInputSUL;
+import de.learnlib.api.oracle.SymbolQueryOracle;
+
+import java.util.Collection;
+
+/**
+ * Not thread safe.
+ *
+ * @param <I>
+ * @param <O>
+ */
+public class StateLocalInputSULSymbolQueryOracle<I, O> implements SymbolQueryOracle<I, O> {
+
+    private final StateLocalInputSUL<I, O> sul;
+    private final O undefinedOutput;
+
+    public StateLocalInputSULSymbolQueryOracle(StateLocalInputSUL<I, O> sul, O undefinedOutput) {
+        this.sul = sul;
+        this.undefinedOutput = undefinedOutput;
+    }
+
+    @Override
+    public O query(I i) {
+        final Collection<I> enabledInputs = sul.currentlyEnabledInputs();
+        if (enabledInputs.contains(i)) {
+            return sul.step(i);
+        } else {
+            return undefinedOutput;
+        }
+    }
+
+    @Override
+    public void reset() {
+        this.sul.post();
+        this.sul.pre();
+    }
+}
