6fc073584b00e5751d527562db5f25171401998c
aleksander-mendoza <45855937+aleksander-mendoza@users.noreply.github.com>
2021-01-03 00:40:37 +0100

    Add OSTIA algorithm (#74)
    * OSTIA sketch
    
    * initial refactoring into separate module
    
    * initial integration of LearnLib/AutomataLib interfaces
    
    * small adjustment in tests
    
    * renaming (Sub)SequentialTransducer
    
    * fixed IllegalArgumentException, onwardForm algorithm, minor refactorization
    
    * add utility classes and provide alternative onward computation
    
    * fixed OSTIA buildPtt procedure to allow for non-sorted informants, minor improvements and optimizations
    
    * cleanups and refactorings
    
    * removed dead code
    * refactored some interfaces/classes
    * added new test cases
    * applied style guidelines
    
    * OSTIA characteristic sample
    
    * some more refactorings
    
    * documentation
    * move some utility to AutomataLib
    * streamlining interfaces
    
    * attempt at handling initial states in onward transformation
    
    * fix onward computation
    
    * some more cleanups
    
    * switch to the development version of AutomataLib
    
    * fixes bug in blue-fringe
    
    * some minor cleanups
    
    * refactoring the `State` class
    * fix issues reported by the code-anylsis tools
    
    * brought back formal guarantees
    
    * aggregate shared data in parent class
    
    * add some more test cases
    
    * make null-analysis happy
    
    Co-authored-by: Alagris <alagris12358@gmail.com>
    Co-authored-by: Markus Frohme <markus.frohme@udo.edu>

---
+++ algorithms/passive/ostia/src/main/java/de/learnlib/algorithms/ostia/OSSTWrapper.java
@@ -0,0 +1,117 @@
+/* Copyright (C) 2013-2020 TU Dortmund
+ * This file is part of LearnLib, http://www.learnlib.de/.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.learnlib.algorithms.ostia;
+
+import java.util.ArrayDeque;
+import java.util.Collection;
+import java.util.LinkedHashSet;
+import java.util.Queue;
+import java.util.Set;
+
+import net.automatalib.automata.transducers.SubsequentialTransducer;
+import net.automatalib.words.Alphabet;
+import net.automatalib.words.Word;
+import net.automatalib.words.WordBuilder;
+import org.checkerframework.checker.nullness.qual.NonNull;
+import org.checkerframework.checker.nullness.qual.Nullable;
+
+/**
+ * @author frohme
+ */
+class OSSTWrapper<I, O> implements SubsequentialTransducer<State, I, Edge, O> {
+
+    private final State root;
+    private final Alphabet<I> inputAlphabet;
+    private final Alphabet<O> outputAlphabet;
+
+    OSSTWrapper(State root, Alphabet<I> inputAlphabet, Alphabet<O> outputAlphabet) {
+        this.root = root;
+        this.inputAlphabet = inputAlphabet;
+        this.outputAlphabet = outputAlphabet;
+    }
+
+    @Override
+    public Collection<State> getStates() {
+        final Set<State> cache = new LinkedHashSet<>();
+        final Queue<State> queue = new ArrayDeque<>();
+
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            @SuppressWarnings("nullness") // false positive https://github.com/typetools/checker-framework/issues/399
+            @NonNull State s = queue.poll();
+            cache.add(s);
+
+            for (Edge transition : s.transitions) {
+                if (transition != null) {
+                    State succ = transition.target;
+
+                    if (succ != null && !cache.contains(succ)) {
+                        queue.add(succ);
+                        cache.add(succ);
+                    }
+                }
+            }
+        }
+
+        return cache;
+    }
+
+    @Override
+    public @Nullable Edge getTransition(State state, I input) {
+        return state.transitions[inputAlphabet.getSymbolIndex(input)];
+    }
+
+    @Override
+    public State getInitialState() {
+        return root;
+    }
+
+    @Override
+    public Word<O> getStateProperty(State state) {
+        return outToWord(state.out);
+    }
+
+    @Override
+    public Word<O> getTransitionProperty(Edge transition) {
+        return outToWord(transition.out);
+    }
+
+    @Override
+    public State getSuccessor(Edge transition) {
+        return transition.target;
+    }
+
+    private Word<O> outToWord(@Nullable Out out) {
+        return outToWord(out == null ? null : out.str);
+    }
+
+    private Word<O> outToWord(@Nullable IntQueue out) {
+        if (out == null) {
+            return Word.epsilon();
+        }
+
+        final WordBuilder<O> wb = new WordBuilder<>();
+
+        IntQueue outIter = out;
+        while (outIter != null) {
+            wb.add(outputAlphabet.getSymbol(outIter.value));
+            outIter = outIter.next;
+        }
+
+        return wb.toWord();
+    }
+}
---
+++ test-support/learner-it-support/src/main/java/de/learnlib/testsupport/it/learner/AbstractSSTPassiveLearnerIT.java
@@ -0,0 +1,90 @@
+/* Copyright (C) 2013-2020 TU Dortmund
+ * This file is part of LearnLib, http://www.learnlib.de/.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.learnlib.testsupport.it.learner;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import de.learnlib.api.query.DefaultQuery;
+import de.learnlib.examples.DefaultPassiveLearningExample;
+import de.learnlib.examples.LearningExample;
+import de.learnlib.examples.LearningExample.MealyLearningExample;
+import de.learnlib.examples.LearningExample.SSTLearningExample;
+import de.learnlib.examples.LearningExamples;
+import de.learnlib.examples.PassiveLearningExample;
+import net.automatalib.automata.UniversalDeterministicAutomaton;
+import net.automatalib.automata.concepts.SuffixOutput;
+import net.automatalib.automata.transducers.SubsequentialTransducer;
+import net.automatalib.words.Alphabet;
+import net.automatalib.words.Word;
+import org.testng.annotations.Factory;
+
+/**
+ * Abstract integration test for passive {@link SubsequentialTransducer}s learning algorithms.
+ * <p>
+ * SST learning algorithms tested by this integration test are expected to assume membership queries yield the full
+ * output word corresponding to the suffix part of the query.
+ *
+ * @author frohme
+ */
+public abstract class AbstractSSTPassiveLearnerIT {
+
+    @Factory
+    public Object[] createExampleITCases() {
+        final List<PassiveLearnerVariantTICase<?, ?, ?>> result = new ArrayList<>();
+
+        for (MealyLearningExample<?, ?> example : LearningExamples.createMealyExamples()) {
+            result.addAll(createAllVariantsITCase(example));
+        }
+
+        for (SSTLearningExample<?, ?> example : LearningExamples.createSSTExamples()) {
+            result.addAll(createAllVariantsITCase(example));
+        }
+
+        return result.toArray();
+    }
+
+    private <I, O, A extends UniversalDeterministicAutomaton<?, I, ?, ?, ?> & SuffixOutput<I, Word<O>>> List<PassiveLearnerVariantTICase<I, Word<O>, SubsequentialTransducer<?, I, ?, O>>> createAllVariantsITCase(
+            LearningExample<I, A> example) {
+
+        final Alphabet<I> alphabet = example.getAlphabet();
+        final A reference = example.getReferenceAutomaton();
+
+        Collection<DefaultQuery<I, Word<O>>> queries = LearnerITUtil.generateSamples(alphabet, reference);
+
+        final PassiveLearnerVariantListImpl<SubsequentialTransducer<?, I, ?, O>, I, Word<O>> variants =
+                new PassiveLearnerVariantListImpl<>();
+        addLearnerVariants(alphabet, variants);
+
+        final PassiveLearningExample<I, Word<O>> effectiveExample =
+                new DefaultPassiveLearningExample<>(queries, alphabet);
+
+        return LearnerITUtil.createPassiveExampleITCases(effectiveExample, variants);
+    }
+
+    /**
+     * Adds, for a given setup, all the variants of the DFA learner to be tested to the specified {@link
+     * PassiveLearnerVariantList variant list}.
+     *
+     * @param alphabet
+     *         the input alphabet
+     * @param variants
+     *         list to add the learner variants to
+     */
+    protected abstract <I, O> void addLearnerVariants(Alphabet<I> alphabet,
+                                                      PassiveLearnerVariantList<SubsequentialTransducer<?, I, ?, O>, I, Word<O>> variants);
+}
