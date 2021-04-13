1515a03f420f1f7bedf71c87df216ccd6899a711
Markus Frohme <markus.frohme@udo.edu>
2021-04-13 14:43:52 +0200

    bump cacio version
    ... which adds support for headless GUI testing on JDK11

--- examples/src/test/java/de/learnlib/examples/ExamplesTest.java
+++ examples/src/test/java/de/learnlib/examples/ExamplesTest.java
@@ -156,8 +156,9 @@ public class ExamplesTest {
     }
 
     private static void checkJVMCompatibility() {
-        if (JVMUtil.getCanonicalSpecVersion() > 8) {
-            throw new SkipException("The headless AWT environment currently only works with Java 8 and below");
+        final int canonicalSpecVersion = JVMUtil.getCanonicalSpecVersion();
+        if (!(canonicalSpecVersion <= 8 || canonicalSpecVersion == 11)) {
+            throw new SkipException("The headless AWT environment currently only works with Java 11 or <=8");
         }
     }
 
