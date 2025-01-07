c458f1c8493b26d4c800361be57cf2930dbe5ff6
Markus Frohme <markus.frohme@udo.edu>
2025-01-07 15:18:53 +0100

    bump license header

--- examples/src/main/java/de/learnlib/example/sli/Example2.java
+++ examples/src/main/java/de/learnlib/example/sli/Example2.java
@@ -1,5 +1,5 @@
-/* Copyright (C) 2013-2024 TU Dortmund University
- * This file is part of LearnLib, http://www.learnlib.de/.
+/* Copyright (C) 2013-2025 TU Dortmund University
+ * This file is part of LearnLib <https://learnlib.de>.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
