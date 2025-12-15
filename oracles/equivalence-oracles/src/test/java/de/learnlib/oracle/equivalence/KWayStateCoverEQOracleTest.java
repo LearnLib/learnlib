88d7cb1bad03d854eaa662f3b48a1921f4bd3ba6
Markus Frohme <mtf90@users.noreply.github.com>
2025-12-15 20:13:30 +0100

    Add equivalence oracles based on k-way state/transition coverage (#156)
    * initial work on kWayOracles
    
    * cleanups
    
    * adjust to AutomataLib + consolidations
    
    * adjust to upstream changes
    
    * reorder attributes
    
    * test the correct oracle
    
    * hide some type variables
    
    * add additional documentation
    
    * cleanups
    
    * adjust configs
    * declutter type information
    
    * do not use special characters in documentation

---
+++ oracles/equivalence-oracles/src/test/java/de/learnlib/oracle/equivalence/KWayStateCoverEQOracleTest.java
@@ -0,0 +1,54 @@
+/* Copyright (C) 2013-2025 TU Dortmund University
+ * This file is part of LearnLib <https://learnlib.de>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.learnlib.oracle.equivalence;
+
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+
+import de.learnlib.TestWordGenerator;
+import net.automatalib.alphabet.Alphabet;
+import net.automatalib.alphabet.impl.Alphabets;
+import net.automatalib.automaton.fsa.DFA;
+import net.automatalib.automaton.fsa.impl.CompactDFA;
+import net.automatalib.common.util.collection.IteratorUtil;
+import net.automatalib.util.automaton.conformance.KWayStateCoverTestsIterator;
+import net.automatalib.util.automaton.random.RandomAutomata;
+import net.automatalib.word.Word;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class KWayStateCoverEQOracleTest {
+
+    private static final int SIZE = 10;
+
+    @Test
+    public void testOracle() {
+        final int seed = 42;
+        final Alphabet<Character> alphabet = Alphabets.characters('a', 'c');
+        final CompactDFA<Character> dfa = RandomAutomata.randomDFA(new Random(seed), SIZE, alphabet);
+
+        final TestWordGenerator<DFA<Integer, Character>, Character> oracle =
+                new KWayStateCoverEQOracleBuilder<DFA<Integer, Character>, Character, Boolean>().withRandom(new Random(
+                        seed)).create();
+
+        List<Word<Character>> tests = oracle.generateTestWords(dfa, alphabet).collect(Collectors.toList());
+        List<Word<Character>> iter =
+                IteratorUtil.list(new KWayStateCoverTestsIterator<>(dfa, alphabet, new Random(seed)));
+
+        Assert.assertEquals(tests, iter);
+    }
+}
